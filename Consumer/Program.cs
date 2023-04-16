cec6e91aacf41c91afeae1d32095995da4d417e0
Мартынов Илья Игоревич <ilya.martynov@osinit.com>
2023-04-16 15:12:56 +0300

    first commit

---
+++ Consumer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory{HostName = "localhost"};
+
+using var conn = factory.CreateConnection();
+
+using var channel = conn.CreateModel();
+
+channel.QueueDeclare(queue:"LetterBox",
+    durable:false,
+    exclusive:false,
+    autoDelete:false,
+    arguments:null);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    System.Console.WriteLine($"Message received: {message}");
+};
+
+channel.BasicConsume(queue:"LetterBox", autoAck:true, consumer:consumer);
+
+Console.ReadKey();
---
+++ Producer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+
+var factory = new ConnectionFactory{HostName = "localhost"};
+
+using var conn = factory.CreateConnection();
+
+using var channel = conn.CreateModel();
+
+channel.QueueDeclare(queue:"LetterBox",
+    durable:false,
+    exclusive:false,
+    autoDelete:false,
+    arguments:null);
+
+var message ="This my first message in rabbitMQ";
+
+var encodingMessage = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish("","LetterBox", null, encodingMessage);
+Console.WriteLine($"Published message: {message}");
